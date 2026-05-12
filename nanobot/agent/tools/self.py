14790897/nanobot

feat(config): add ModelPresetConfig and runtime preset switching
- Add `ModelPresetConfig` schema for named model presets
- Add `model_presets` dict to `Config` and `model_preset` field to `AgentDefaults`
- Add `resolve_preset()` to return effective model params from preset or defaults
- Add `@model_validator` to reject unknown preset names
- Update `_match_provider()` to use resolved preset model/provider
- Update `make_provider()` and `provider_signature()` to use `resolve_preset()`
- Add `model_preset` property to `AgentLoop` for atomic runtime switching
- Update `AgentLoop.from_config()` to inject a runtime `default` preset
- Wire self-tool to inspect/clear preset state
- Update CLI display strings to show active preset

nanobot/agent/tools/self.py
@@ -330,6 +330,8 @@ class MyTool(Tool):
         # RESTRICTED keys
         for k in self.RESTRICTED:
             parts.append(self._format_value(getattr(loop, k, None), k))
+        # model_preset (property on AgentLoop)
+        parts.append(self._format_value(loop.model_preset, "model_preset"))
         # Other useful top-level keys shown in description
         for k in ("workspace", "provider_retry_mode", "max_tool_result_chars", "_current_iteration", "web_config", "exec_config", "subagents"):
             if _has_real_attr(loop, k):
@@ -386,7 +388,11 @@ class MyTool(Tool):
                 value = expected(value)
             except (ValueError, TypeError):
                 return f"Error: '{key}' must be {expected.__name__}, got {type(value).__name__}"
+        # --- existing restricted key logic ---
         old = getattr(self._loop, key)
+        # When model is set directly, it no longer matches any preset
+        if key == "model":
+            self._loop._active_preset = None
         if "min" in spec and value < spec["min"]:
             return f"Error: '{key}' must be >= {spec['min']}"
         if "max" in spec and value > spec["max"]:
@@ -412,7 +418,11 @@ class MyTool(Tool):
                         f"REJECTED type mismatch {key}: expects {old_t.__name__}, got {new_t.__name__}",
                     )
                     return f"Error: '{key}' expects {old_t.__name__}, got {new_t.__name__}"
-            setattr(self._loop, key, value)
+            try:
+                setattr(self._loop, key, value)
+            except (ValueError, KeyError) as e:
+                self._audit("modify", f"REJECTED {key}: {e}")
+                return f"Error: {e}"
             self._audit("modify", f"{key}: {old!r} -> {value!r}")
             return f"Set {key} = {value!r} (was {old!r})"
         if callable(value):

tests/agent/test_self_model_preset.py
@@ -1,0 +1,134 @@
+from types import SimpleNamespace
+from unittest.mock import MagicMock
+
+import pytest
+
+from nanobot.agent.loop import AgentLoop
+from nanobot.agent.tools.self import MyTool
+from nanobot.bus.queue import MessageBus
+from nanobot.config.schema import ModelPresetConfig
+
+
+def _provider(default_model: str, max_tokens: int = 123) -> MagicMock:
+    provider = MagicMock()
+    provider.get_default_model.return_value = default_model
+    provider.generation = SimpleNamespace(
+        max_tokens=max_tokens, temperature=0.1, reasoning_effort=None
+    )
+    return provider
+
+
+def _make_loop(tmp_path, presets=None, active_preset=None):
+    provider = _provider("base-model")
+    return AgentLoop(
+        bus=MessageBus(),
+        provider=provider,
+        workspace=tmp_path,
+        model="base-model",
+        context_window_tokens=1000,
+        model_presets=presets or {},
+        model_preset=active_preset,
+    )
+
+
+def test_model_preset_getter_none_when_not_set(tmp_path) -> None:
+    loop = _make_loop(tmp_path)
+    assert loop.model_preset is None
+
+
+def test_model_preset_setter_updates_state(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(
+            model="openai/gpt-4.1",
+            provider="openai",
+            max_tokens=4096,
+            context_window_tokens=32_768,
+            temperature=0.5,
+            reasoning_effort="low",
+        )
+    }
+    loop = _make_loop(tmp_path, presets=presets)
+    loop.model_preset = "fast"
+
+    assert loop.model_preset == "fast"
+    assert loop.model == "openai/gpt-4.1"
+    assert loop.context_window_tokens == 32_768
+    assert loop.provider.generation.temperature == 0.5
+    assert loop.provider.generation.max_tokens == 4096
+    assert loop.provider.generation.reasoning_effort == "low"
+
+
+def test_model_preset_setter_raises_on_unknown(tmp_path) -> None:
+    loop = _make_loop(tmp_path)
+    with pytest.raises(KeyError, match="model_preset 'missing' not found"):
+        loop.model_preset = "missing"
+
+
+def test_model_preset_setter_raises_on_empty_string(tmp_path) -> None:
+    loop = _make_loop(tmp_path)
+    with pytest.raises(ValueError, match="model_preset must be a non-empty string"):
+        loop.model_preset = ""
+
+
+def test_self_tool_inspect_shows_model_preset(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(model="openai/gpt-4.1"),
+    }
+    loop = _make_loop(tmp_path, presets=presets, active_preset="fast")
+    tool = MyTool(loop=loop, modify_allowed=True)
+    output = tool._inspect_all()
+    assert "model_preset: 'fast'" in output
+
+
+def test_self_tool_set_model_preset_via_modify(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(model="openai/gpt-4.1"),
+    }
+    loop = _make_loop(tmp_path, presets=presets)
+    tool = MyTool(loop=loop, modify_allowed=True)
+    result = tool._modify("model_preset", "fast")
+    # model_preset is not in RESTRICTED, so it goes through _modify_free
+    # which calls setattr(loop, "model_preset", "fast")
+    assert "Error" not in result
+    assert loop.model_preset == "fast"
+    assert loop.model == "openai/gpt-4.1"
+
+
+def test_self_tool_set_model_clears_active_preset(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(model="openai/gpt-4.1"),
+    }
+    loop = _make_loop(tmp_path, presets=presets, active_preset="fast")
+    tool = MyTool(loop=loop, modify_allowed=True)
+    result = tool._modify("model", "anthropic/claude-opus-4-5")
+    assert "Error" not in result
+    assert loop._active_preset is None
+    assert loop.model == "anthropic/claude-opus-4-5"
+
+
+def test_from_config_injects_default_preset(tmp_path) -> None:
+    from unittest.mock import patch
+    from nanobot.config.schema import Config
+    config = Config.model_validate({
+        "agents": {"defaults": {"model": "openai/gpt-4.1", "workspace": str(tmp_path)}},
+    })
+    fake_provider = _provider("openai/gpt-4.1")
+    with patch("nanobot.providers.factory.make_provider", return_value=fake_provider):
+        loop = AgentLoop.from_config(config)
+    assert "default" in loop.model_presets
+    assert loop.model_presets["default"].model == "openai/gpt-4.1"
+
+
+def test_from_config_preserves_existing_default_preset(tmp_path) -> None:
+    from unittest.mock import patch
+    from nanobot.config.schema import Config
+    config = Config.model_validate({
+        "agents": {"defaults": {"model": "openai/gpt-4.1", "workspace": str(tmp_path)}},
+        "model_presets": {
+            "default": {"model": "custom-model"}
+        },
+    })
+    fake_provider = _provider("openai/gpt-4.1")
+    with patch("nanobot.providers.factory.make_provider", return_value=fake_provider):
+        loop = AgentLoop.from_config(config)
+    assert loop.model_presets["default"].model == "custom-model"

CrazyForks/nanobot-2

nanobot/agent/tools/self.py
@@ -347,6 +347,7 @@ class MyTool(Tool, ContextAware):
         # RESTRICTED keys
         for k in self.RESTRICTED:
             parts.append(self._format_value(getattr(state, k, None), k))
+        parts.append(self._format_value(state.model_preset, "model_preset"))
         # Other useful top-level keys shown in description
         for k in ("workspace", "provider_retry_mode", "max_tool_result_chars", "_current_iteration", "web_config", "exec_config", "subagents"):
             if _has_real_attr(state, k):
@@ -411,6 +412,8 @@ class MyTool(Tool, ContextAware):
         if "min_len" in spec and len(str(value)) < spec["min_len"]:
             return f"Error: '{key}' must be at least {spec['min_len']} characters"
         setattr(self._runtime_state, key, value)
+        if key == "model":
+            self._runtime_state._active_preset = None
         if key == "max_iterations" and hasattr(self._runtime_state, "_sync_subagent_runtime_limits"):
             self._runtime_state._sync_subagent_runtime_limits()
         self._audit("modify", f"{key}: {old!r} -> {value!r}")
@@ -429,7 +432,11 @@ class MyTool(Tool, ContextAware):
                         f"REJECTED type mismatch {key}: expects {old_t.__name__}, got {new_t.__name__}",
                     )
                     return f"Error: '{key}' expects {old_t.__name__}, got {new_t.__name__}"
-            setattr(self._runtime_state, key, value)
+            try:
+                setattr(self._runtime_state, key, value)
+            except (ValueError, KeyError) as e:
+                self._audit("modify", f"REJECTED {key}: {e}")
+                return f"Error: {e}"
             self._audit("modify", f"{key}: {old!r} -> {value!r}")
             return f"Set {key} = {value!r} (was {old!r})"
         if callable(value):

tests/agent/test_self_model_preset.py
@@ -1,0 +1,134 @@
+from types import SimpleNamespace
+from unittest.mock import MagicMock
+
+import pytest
+
+from nanobot.agent.loop import AgentLoop
+from nanobot.agent.tools.self import MyTool
+from nanobot.bus.queue import MessageBus
+from nanobot.config.schema import ModelPresetConfig
+
+
+def _provider(default_model: str, max_tokens: int = 123) -> MagicMock:
+    provider = MagicMock()
+    provider.get_default_model.return_value = default_model
+    provider.generation = SimpleNamespace(
+        max_tokens=max_tokens, temperature=0.1, reasoning_effort=None
+    )
+    return provider
+
+
+def _make_loop(tmp_path, presets=None, active_preset=None):
+    provider = _provider("base-model")
+    return AgentLoop(
+        bus=MessageBus(),
+        provider=provider,
+        workspace=tmp_path,
+        model="base-model",
+        context_window_tokens=1000,
+        model_presets=presets or {},
+        model_preset=active_preset,
+    )
+
+
+def test_model_preset_getter_none_when_not_set(tmp_path) -> None:
+    loop = _make_loop(tmp_path)
+    assert loop.model_preset is None
+
+
+def test_model_preset_setter_updates_state(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(
+            model="openai/gpt-4.1",
+            provider="openai",
+            max_tokens=4096,
+            context_window_tokens=32_768,
+            temperature=0.5,
+            reasoning_effort="low",
+        )
+    }
+    loop = _make_loop(tmp_path, presets=presets)
+    loop.model_preset = "fast"
+
+    assert loop.model_preset == "fast"
+    assert loop.model == "openai/gpt-4.1"
+    assert loop.context_window_tokens == 32_768
+    assert loop.provider.generation.temperature == 0.5
+    assert loop.provider.generation.max_tokens == 4096
+    assert loop.provider.generation.reasoning_effort == "low"
+
+
+def test_model_preset_setter_raises_on_unknown(tmp_path) -> None:
+    loop = _make_loop(tmp_path)
+    with pytest.raises(KeyError, match="model_preset 'missing' not found"):
+        loop.model_preset = "missing"
+
+
+def test_model_preset_setter_raises_on_empty_string(tmp_path) -> None:
+    loop = _make_loop(tmp_path)
+    with pytest.raises(ValueError, match="model_preset must be a non-empty string"):
+        loop.model_preset = ""
+
+
+def test_self_tool_inspect_shows_model_preset(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(model="openai/gpt-4.1"),
+    }
+    loop = _make_loop(tmp_path, presets=presets, active_preset="fast")
+    tool = MyTool(runtime_state=loop, modify_allowed=True)
+    output = tool._inspect_all()
+    assert "model_preset: 'fast'" in output
+
+
+def test_self_tool_set_model_preset_via_modify(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(model="openai/gpt-4.1"),
+    }
+    loop = _make_loop(tmp_path, presets=presets)
+    tool = MyTool(runtime_state=loop, modify_allowed=True)
+    result = tool._modify("model_preset", "fast")
+    assert "Error" not in result
+    assert loop.model_preset == "fast"
+    assert loop.model == "openai/gpt-4.1"
+
+
+def test_self_tool_set_model_clears_active_preset(tmp_path) -> None:
+    presets = {
+        "fast": ModelPresetConfig(model="openai/gpt-4.1"),
+    }
+    loop = _make_loop(tmp_path, presets=presets, active_preset="fast")
+    tool = MyTool(runtime_state=loop, modify_allowed=True)
+    result = tool._modify("model", "anthropic/claude-opus-4-5")
+    assert "Error" not in result
+    assert loop._active_preset is None
+    assert loop.model == "anthropic/claude-opus-4-5"
+
+
+def test_from_config_injects_default_preset(tmp_path) -> None:
+    from unittest.mock import patch
+
+    from nanobot.config.schema import Config
+    config = Config.model_validate({
+        "agents": {"defaults": {"model": "openai/gpt-4.1", "workspace": str(tmp_path)}},
+    })
+    fake_provider = _provider("openai/gpt-4.1")
+    with patch("nanobot.providers.factory.make_provider", return_value=fake_provider):
+        loop = AgentLoop.from_config(config)
+    assert "default" in loop.model_presets
+    assert loop.model_presets["default"].model == "openai/gpt-4.1"
+
+
+def test_from_config_preserves_existing_default_preset(tmp_path) -> None:
+    from unittest.mock import patch
+
+    from nanobot.config.schema import Config
+    config = Config.model_validate({
+        "agents": {"defaults": {"model": "openai/gpt-4.1", "workspace": str(tmp_path)}},
+        "model_presets": {
+            "default": {"model": "custom-model"}
+        },
+    })
+    fake_provider = _provider("openai/gpt-4.1")
+    with patch("nanobot.providers.factory.make_provider", return_value=fake_provider):
+        loop = AgentLoop.from_config(config)
+    assert loop.model_presets["default"].model == "custom-model"